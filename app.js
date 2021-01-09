e8415612eaef02bb3db904c6174e39e41ed85188
Taichi Sato <okms310gmail.com>
2021-01-09 23:10:18 +0900

    add ejs

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello nodejs");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render("index", {message: "うおおおおおお"})
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server started port ${port}`));
