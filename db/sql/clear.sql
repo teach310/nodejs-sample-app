5714e6dfef89b337517db221412b8b3bcd6fb8f5
Taichi Sato <okms310gmail.com>
2021-03-20 15:36:56 +0900

    seedデータをいれる仕組み作成

---
+++ db/sql/clear.sql
@@ -0,0 +1,2 @@
+USE sample;
+DELETE FROM users;
---
+++ db/sql/seed.sql
@@ -0,0 +1,6 @@
+USE sample;
+INSERT INTO users
+VALUES
+  (1, 'Alice', 'alice@example.com', 'Password1'),
+  (2, 'Bob', 'bob@example.com', 'Password1'),
+  (3, 'Carol', 'carol@example.com', 'Password1');
