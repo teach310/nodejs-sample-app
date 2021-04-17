b991ff7ac775bb3e15985f9b2e6a19b0b178b80c
yuki ozawa <0zawayuki.aug@gmail.com>
2021-04-17 16:29:28 +0900

    メニューの更新機能の追加

--- app.js
+++ app.js
@@ -57,6 +57,36 @@ app.post('/items/create', (req, res) =>{
   )
 })
 
+app.post('/items/delete/:id', (req, res) =>{
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/items');
+    }
+  );
+});
+
+app.get('/items/edit/:id',(req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) =>{
+      res.render('items/edit.ejs',{item: results[0]});
+    }
+  );
+});
+
+app.post('/items/update/:id', (req, res) =>{
+  connection.query(
+    'UPDATE items SET name = ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error,results) =>{
+      res.redirect('/items')
+    }
+  );
+});
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server started port ${port}`));
 
